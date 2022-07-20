e37641a2035c84b48cc139846218407874336d33
Hp <wadgaonkar7744@gmail.com>
2022-07-20 19:45:52 +0530

    finished project

---
+++ app/src/main/java/com/example/conveter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.conveter;
+
+import static java.lang.Double.parseDouble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edittext;
+    TextView textview, textView2, textView4, textView3;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textview = findViewById(R.id.textView2);
+        textView2 = findViewById(R.id.textView4);
+        textView4 = findViewById(R.id.textView3);
+        edittext = findViewById(R.id.editTextNumber);
+        textView3 = findViewById(R.id.textView3);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConvertFromKiloToPound();
+            }
+        });
+    }
+
+    private void ConvertFromKiloToPound() {
+        String ValueEnteredinKilo = edittext.getText().toString();
+        double kilo = parseDouble(ValueEnteredinKilo);
+        double pounds = kilo * 2.205;
+        textView3.setText(" "+ pounds);
+    }
+}
+
+
+
+
+
